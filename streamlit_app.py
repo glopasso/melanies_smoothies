f945adb2abce03966c0950cf5fd1a1b10153346a
Guilherme Lopasso <33458625+glopasso@users.noreply.github.com>
2025-03-31 12:44:10 -0300

    Update streamlit_app.py
    include request to smothiesfroot.com

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,10 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:'
                                  , my_dataframe
                                   ,max_selections=5
                                  )
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 if ingredients_list:
 
     ingredients_string =''
